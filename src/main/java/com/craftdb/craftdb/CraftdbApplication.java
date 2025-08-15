fd0c39b7c5424ff96786469d158e6145f5b9dad3
shubhamjain2908 <shubhamjain2908@gmail.com>
2025-08-15 14:25:42 +0530

    init commit

---
+++ src/main/java/com/craftdb/craftdb/CraftdbApplication.java
@@ -0,0 +1,13 @@
+package com.craftdb.craftdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftdbApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CraftdbApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/craftdb/craftdb/CraftdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.craftdb.craftdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftdbApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
